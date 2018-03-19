18312be42b977cfe099ce4084c0e8e55f8decc0a
Benjamin Heppenstall <37471480+BenjaminHeppenstall@users.noreply.github.com>
2018-03-19 21:25:54 +0100

    new style

--- aufgabe 16/www/js/myscripts.js
+++ aufgabe 16/www/js/myscripts.js
@@ -1,60 +1,114 @@
+//once button is pressed: 
 function everything() {
+    //create variable to store user input 
     var answerInput = prompt("bitte q eingeben", "8");
 
+    if (document.getElementById("demo1")) {
+        document.getElementById('demo1').style.display='none';
+        document.getElementById('attentionImage').style.display='none';
+    } 
+
+
+
+    //if user doesnt press cancel do:
     if (answerInput != null) {
-        var a = parseInt(answerInput, 10);
-        var forAll = "";
-        var forMax = "";
-        var nummern = [];
- 
-        function f(q,x) {
-            var y = (30 - (-(q/10) * x + q)) * (40 - x);
-            return y;
+
+        var a = parseInt(answerInput, 10);                               
+        var forAll = "";                                                  
+        var forMax = "";                                                
+        var nummern = [];                                             
+
+        function isItNumber(h) {
+            if (Number.isInteger(h)) { 
+                
+
+
+                function f(q,x) {
+                    var y = (30 - (-(q/10) * x + q)) * (40 - x);                 
+                    return y;                                                    
+                }                                                     
+                
+
+                for(var i = 0; i < 1000; i++) {                            
+                    var b = 0 + i * 0.1; 
+                    forAll += Math.round((b + f(a,b)) * 1000) / 1000 + "<br>";  
+                    forMax = b + f(a,b);                                        
+                    nummern.push(forMax)                                                                          
+                    var maxNummern = Math.max.apply(null, nummern) 
+                }               
+
+                var createMaxNum = document.createElement("p");   
+                createMaxNum.id = "max";                                         
+                var createMaxNumContent = document.createTextNode("");           
+                createMaxNum.appendChild(createMaxNumContent);                  
+                var element1 = document.getElementById("div1");                  
+                element1.appendChild(createMaxNum);                              
+
+                var createAllNum = document.createElement("p");                  
+                createAllNum.id = "all";                                         
+                var createAllNumContent = document.createTextNode("");           
+                createAllNum.appendChild(createAllNumContent);                   
+                var element2 = document.getElementById("div1");                  
+                element2.appendChild(createAllNum);                              
+
+                document.getElementById("max").innerHTML = "<br>" + "Größtmöglicher Flächeninhalt bei " + a + "cm:" + "<br>" + "<b>" + "<br>" + 
+                Math.round(maxNummern * 1000) / 1000 + " cm²" + "</b>" + "<br>" + "<br>";
+                document.getElementById("all").innerHTML = "<br>" + "Andere Punkte auf der Parabel:" + "<br>" + "<br>" + forAll;
+
+                var elem = document.getElementById('qEingeben');               
+                elem.parentNode.removeChild(elem);                  
+
+                var button = document.createElement("button");                      
+                button.id = "cancelButton";
+                button.class = "button"                                                               
+                button.innerHTML = "erneut versuchen";                                     
+                var parentEl = document.getElementById("fillIn");                
+                parentEl.appendChild(button);                                   
+
+                button.addEventListener ("click", function() {                  
+                    location.reload(false);  
+                });                                    
+            } else {
+
+                var div = document.createElement("div");
+                div.id = "attentionDiv";
+
+                document.getElementById("allNum").appendChild(div);
+
+                var myImg = document.createElement("IMG");
+                myImg.src = "images/attention.png";
+                myImg.setAttribute("alt", "attention");
+                myImg.id = "attentionImage";
+                var element4 = document.getElementById("attentionDiv")
+                element4.appendChild(myImg); 
+            
+                //div.innerHTML = "Bitte eine <i>gültige</i> Nummer eingeben  ";
+
+                //document.getElementById("allNum").appendChild(div);
+
+                /*var myImg = document.createElement("IMG");
+                myImg.src = "images/attention.png";
+                myImg.setAttribute("width", "120");
+                myImg.setAttribute("height", "88");
+                myImg.setAttribute("alt", "attention");
+                myImg.id = "attentionImage";
+                var element4 = document.getElementById("attentionDiv")
+                element4.appendChild(myImg); */
+
+                
+
+                var invalidInput = document.createElement("p");                  
+                invalidInput.id = "demo1";                                         
+                var invalidInputContent = document.createTextNode("");           
+                invalidInput.appendChild(invalidInputContent);                   
+                var element3 = document.getElementById("attentionDiv");                  
+                element3.appendChild(invalidInput); 
+                
+
+                document.getElementById("demo1").innerHTML = "Bitte eine <i>gültige</i> Nummer eingeben" 
+            }
         }
 
-        for(var i = 0; i < 1000; i++) {
-            var b = 0 + i * 0.1;
-            forAll += Math.round((b + f(a,b)) * 1000) / 1000 + "<br>";
-            forMax = b + f(a,b);
-            nummern.push(forMax)
-            var maxNummern = Math.max.apply(null, nummern)
-        }
-
-
-        //create element to store maximum number
-        var createMaxNum = document.createElement("p");                  //create element
-        createMaxNum.id = "max";                                         //give element id
-        var createMaxNumContent = document.createTextNode("");           //create empty text to overwrite later          //works
-        createMaxNum.appendChild(createMaxNumContent);                   //give element text
-        var element1 = document.getElementById("div1");                  //get parent element
-        element1.appendChild(createMaxNum);                              //append child to parent element
-
-        //create element to store all other numbers
-        var createAllNum = document.createElement("p");                  //create element
-        createAllNum.id = "all";                                         //give element id
-        var createAllNumContent = document.createTextNode("");           //create empty text to overwrite later          //works
-        createAllNum.appendChild(createAllNumContent);                   //give element text
-        var element2 = document.getElementById("div1");                  //get parent element
-        element2.appendChild(createAllNum);                              //append child to parent element
-
-        //write max and all into created elements
-        document.getElementById("max").innerHTML = "<br>" + "Größtmöglicher Flächeninhalt bei " + a + "cm:" + "<br>" + "<b>" + "<br>" + 
-        Math.round(maxNummern * 1000) / 1000 + " cm²" + "</b>" + "<br>" + "<br>";
-        document.getElementById("all").innerHTML = "<br>" + "Andere Punkte auf der Parabel:" + "<br>" + "<br>" + forAll;
-        
-        //remove original input button
-        var elem = document.getElementById('qEingeben');                 //get element to remove                         //works
-        elem.parentNode.removeChild(elem);                               //remove element
-        
-        //create cancel buttton
-        var button = document.createElement("button");                   //create button    
-        button.id = "cancelButton";                                      //give el id                          
-        button.innerHTML = "cancel";                                     //write into button                             //works
-        var parentEl = document.getElementById("fillIn");                //get parent Element
-        parentEl.appendChild(button);                                    //append into parent element
-
-        button.addEventListener ("click", function() {                   //give button function
-            location.reload(false);                                      //reload page
-        });
+        isItNumber(a);
     }
 }
